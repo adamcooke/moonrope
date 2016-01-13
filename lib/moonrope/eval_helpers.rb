147dce012e61c540c2ab652e69f26eb2aaabf5c6
Adam Cooke <adam@atechmedia.com>
2016-01-13 16:41:11 +0000

    add a default page number in the paginated helper

--- lib/moonrope/eval_helpers.rb
+++ lib/moonrope/eval_helpers.rb
@@ -40,8 +40,8 @@ module Moonrope
     def paginate(collection, max_per_page = 60, &block)
       per_page = params.per_page || 30
       per_page = max_per_page if per_page < 1 || per_page > max_per_page
-      paginated_results = collection.page(params.page).per(per_page)
-      set_flag :paginated, {:page => params.page, :per_page => per_page, :total_pages => paginated_results.total_pages, :total_records => paginated_results.total_count}
+      paginated_results = collection.page(params.page || 1).per(per_page)
+      set_flag :paginated, {:page => params.page || 1, :per_page => per_page, :total_pages => paginated_results.total_pages, :total_records => paginated_results.total_count}
       paginated_results.to_a.map do |result|
         block.call(result)
       end
