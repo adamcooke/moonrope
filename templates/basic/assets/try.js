3dd3e9f624aeb56c6a41615521545469356e8594
Adam Cooke <adam@atechmedia.com>
2016-02-08 21:33:46 +0000

    tidying js for requests

--- templates/basic/assets/try.js
+++ templates/basic/assets/try.js
@@ -1,11 +1,22 @@
 $(document).ready(function() {
+  //
+  // Form submission
+  //
   $('form').on('submit', function() {
+
+    // Gets values used to make up the URL which should be
+    // requested for this request.
     var host = $("input[name=host]", $(this)).val()
     var version = $("input[name=version]", $(this)).val()
     var controller = $("input[name=controller]", $(this)).val()
     var action = $("input[name=action]", $(this)).val()
+    var url = host + "/api/" + version + "/" + controller + "/" + action
+    // Get all fields which will be added as headers
     var headerFields = $('input.headerField', $(this))
-    var data = {}
+    // Get the output box ready for use
+    var outputBox = $('.tryForm__output', $(this))
+    // Create a hash fo all parameters which will be submitted
+    var parameters = {}
     $('input.paramField').each(function(){
       $this = $(this)
       value = $this.val()
@@ -13,20 +24,20 @@ $(document).ready(function() {
       name = $this.attr('name')
       if(value.length) {
         if(type == 'Integer') {
-          data[name] = parseInt(value);
+          parameters[name] = parseInt(value);
         } else {
-          data[name] = value;
+          parameters[name] = value;
         }
       }
     });
-    var outputBox = $('.tryForm__output', $(this))
-    var url = host + "/api/" + version + "/" + controller + "/" + action
+    // Make the AJAX request
     $.ajax({
       url: url,
       method: 'POST',
       contentType: 'application/json',
-      data: JSON.stringify(data),
+      data: JSON.stringify(parameters),
       beforeSend: function(xhr) {
+        // Add any headers which have been added
         headerFields.each(function() {
           $field = $(this)
           value = $field.val()
@@ -36,7 +47,8 @@ $(document).ready(function() {
         })
       },
       success: function(data) {
-
+        // Success means that we got a 200 OK which means we can be pretty
+        // sure that we've got a moonrope response.
         if(data.status == "success") {
           outputBox.addClass('tryForm__output--success').removeClass('tryForm__output--error')
         } else {
@@ -46,6 +58,8 @@ $(document).ready(function() {
         outputBox.show()
       },
       error: function() {
+        // Errors which occurr aren't very well reported at the moment.
+        // They should be.
         outputBox.show()
         outputBox.text("Failed to make request.")
         outputBox.addClass('tryForm__output--error').removeClass('tryForm__output--success')
@@ -54,6 +68,9 @@ $(document).ready(function() {
     return false
   });
 
+  //
+  // Open the try form
+  //
   $('p.tryFormActivate a').on('click', function() {
     $form = $('form.tryForm')
     $parent = $(this).parents('p')
@@ -62,6 +79,9 @@ $(document).ready(function() {
     return false
   });
 
+  //
+  // Close the try form
+  //
   $('button.tryFormCancel').on('click', function() {
     $form = $('form.tryForm')
     $parent = $('p.tryFormActivate')
@@ -69,4 +89,4 @@ $(document).ready(function() {
     $parent.show()
     return false
   });
-})
+});
